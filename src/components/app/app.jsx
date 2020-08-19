e8e706015cffa6955bba7c3fe00b4612e5551005
Uporold <crubick1337@gmail.com>
2020-08-19 21:24:33 +0300

    Отрисовка фильмов из props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Main from "../main/main";
 
-function App() {
-  return <Main />;
+function App({ movies }) {
+  return <Main movies={movies} />;
 }
+
+App.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+};
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 
+const fakeMoviesList = [`Batman`, `Lord Of The Rings`, `Star Wars`, `Deadpool`];
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // eslint-disable-next-line react/prop-types
+  <App movies={fakeMoviesList} />,
   document.getElementById("root")
 );
