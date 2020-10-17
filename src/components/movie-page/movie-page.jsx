ecccb4f09a6db0a4d9d1427f01ef73f16f96dd01
Uporold <crubick1337@gmail.com>
2020-10-17 17:11:15 +0300

    Компонент header.

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -9,6 +9,7 @@ import MoviesList from "../movies-list/movies-list";
 import { movieNavs } from "../../utilities/util";
 import { projectPropTypes } from "../../utilities/project-prop-types";
 import Footer from "../footer/footer";
+import Header from "../header/header";
 
 class MoviePage extends PureComponent {
   constructor(props) {
@@ -84,26 +85,7 @@ class MoviePage extends PureComponent {
 
             <h1 className="visually-hidden">WTW</h1>
 
-            <header className="page-header movie-card__head">
-              <div className="logo">
-                <a href="main.html" className="logo__link">
-                  <span className="logo__letter logo__letter--1">W</span>
-                  <span className="logo__letter logo__letter--2">T</span>
-                  <span className="logo__letter logo__letter--3">W</span>
-                </a>
-              </div>
-
-              <div className="user-block">
-                <div className="user-block__avatar">
-                  <img
-                    src="/img/avatar.jpg"
-                    alt="User avatar"
-                    width="63"
-                    height="63"
-                  />
-                </div>
-              </div>
-            </header>
+            <Header />
 
             <div className="movie-card__wrap">
               <div className="movie-card__desc">
