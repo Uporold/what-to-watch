72a5f72b3e1dd385d7b2a05a7aa3040818654efb
Uporold <crubick1337@gmail.com>
2020-10-23 11:44:35 +0300

    Редирект при повторном наборе /login если пользователь уже аутентифицировался.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import MoviePage from "../movie-page/movie-page";
@@ -9,8 +9,9 @@ import history from "../../history";
 import { getLoadingStatus } from "../../redux/data/selectors";
 import LoaderSpinner from "../loader-spinner/loader-spinner";
 import SignIn from "../sign-in/sign-in";
+import { getAuthorizationStatus } from "../../redux/user/selectors";
 
-const App = ({ isDataLoading }) => {
+const App = ({ isDataLoading, authorizationStatus }) => {
   return (
     <>
       {!isDataLoading ? (
@@ -19,7 +20,13 @@ const App = ({ isDataLoading }) => {
             <Route exact path="/" component={Main} />
             <Route exact path="/films/:id" component={MoviePage} />
             <Route exact path="/player/:id" component={VideoPlayer} />
-            <Route exact path="/login" component={SignIn} />
+            <Route
+              exact
+              path="/login"
+              render={() => {
+                return !authorizationStatus ? <SignIn /> : <Redirect to="/" />;
+              }}
+            />
           </Switch>
         </Router>
       ) : (
@@ -31,10 +38,12 @@ const App = ({ isDataLoading }) => {
 
 const mapStateToProps = (state) => ({
   isDataLoading: getLoadingStatus(state),
+  authorizationStatus: getAuthorizationStatus(state),
 });
 
 App.propTypes = {
   isDataLoading: PropTypes.bool.isRequired,
+  authorizationStatus: PropTypes.bool.isRequired,
 };
 
 export { App };
