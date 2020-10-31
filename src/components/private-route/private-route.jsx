9bcf26a18e82977f04d83c8830e4a484f8c51f8c
Uporold <crubick1337@gmail.com>
2020-10-31 18:31:11 +0300

    Компонент private-route.jsx

---
+++ src/components/private-route/private-route.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { getAuthorizationStatus } from "../../redux/user/selectors";
+
+const PrivateRoute = (props) => {
+  const { render, path, exact, authorizationStatus } = props;
+
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(routeProps) => {
+        return authorizationStatus ? render(routeProps) : <Redirect to="/" />;
+      }}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+  exact: PropTypes.bool.isRequired,
+  path: PropTypes.string.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: getAuthorizationStatus(state),
+});
+
+export { PrivateRoute };
+export default connect(mapStateToProps)(PrivateRoute);
