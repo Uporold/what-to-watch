7445fff8834bb95344bacf3d8b33451c88617f42
Uporold <crubick1337@gmail.com>
2021-02-11 16:57:17 +0300

    TS: Api

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,19 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 
-export const createAPI = (onUnauthorized) => {
+export const createAPI = (
+  onUnauthorized: (err: AxiosError) => void,
+): AxiosInstance => {
   const api = axios.create({
     baseURL: `https://5.react.pages.academy/wtw`,
     timeout: 1000 * 5,
     withCredentials: true,
   });
 
-  const onSuccess = (response) => {
+  const onSuccess = (response: AxiosResponse) => {
     return response;
   };
 
-  const onFail = (err) => {
+  const onFail = (err: AxiosError) => {
     onUnauthorized(err);
 
     throw err;
