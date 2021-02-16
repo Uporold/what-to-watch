0da7ae7026a314fd0c0cc6119dae1bbf3b13f592
Uporold <crubick1337@gmail.com>
2021-02-16 15:15:06 +0300

    AllReduxActions

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,9 +1,9 @@
 import { Action, combineReducers } from "redux";
 import { AxiosInstance } from "axios";
 import { ThunkAction } from "redux-thunk";
-import { reducer as data } from "./data/data";
-import { reducer as app } from "./app/app";
-import { reducer as user } from "./user/user";
+import { reducer as data, ActionCreator as DataActions } from "./data/data";
+import { reducer as app, ActionCreator as AppActions } from "./app/app";
+import { reducer as user, ActionCreator as UserActions } from "./user/user";
 
 export const rootReducer = combineReducers({
   DATA: data,
@@ -12,6 +12,11 @@ export const rootReducer = combineReducers({
 });
 
 export type GlobalState = ReturnType<typeof rootReducer>;
+
+const combinedActions = { ...DataActions, ...AppActions, ...UserActions };
+export type AllReduxActions = ReturnType<
+  InferActionsTypes<typeof combinedActions>
+>;
 
 export type InferActionsTypes<T> = T extends { [key: string]: infer U }
   ? U
